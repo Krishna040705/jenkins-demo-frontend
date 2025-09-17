77ab43e210a639d816dde0b633bdd6c566cab1a5
Keerti Krishna Sreenivas S <2300031039@kluniversity.in>
2025-09-17 10:36:48 +0530

    commit5

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/jenkins-demo-frontend/',
+  base: '/jenkins-demo-frontend',
   plugins: [react()],
 })
